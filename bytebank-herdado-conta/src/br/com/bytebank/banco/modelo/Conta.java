fcef7cb094da92fe2da55288516cab952013f273
ThalliaB <thalliabrum60@gmail.com>
2021-09-27 09:55:27 -0300

    Add files modified with test file of java.util

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -6,7 +6,7 @@ package br.com.bytebank.banco.modelo;
  * @author thall
  *
  */
-public abstract class Conta {
+public abstract class Conta extends Object implements Comparable<Conta>{
 
 	protected double saldo;
 	private int agencia;
@@ -110,8 +110,14 @@ public abstract class Conta {
 	}
 	
 	@Override
+	public int compareTo(Conta outra) {
+		
+		return Double.compare(this.saldo, outra.saldo);
+	}
+	
+	@Override
 	public String toString() {
-		return "Numero:" + this.numero + "; " + "Agência: " + this.agencia;
+		return "Numero:" + this.numero + "; " + "Agência: " + this.agencia + " Saldo: " + this.saldo;
 	}
 
 }
